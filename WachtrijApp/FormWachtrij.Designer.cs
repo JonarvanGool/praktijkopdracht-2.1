05894566bbed07b74cf7647499046067daf97038
AxelScherbeijn <D283948@edu.curio.nl>
2021-02-25 14:16:41 +0100

    Thema van het programma
    de kleur en text style is aangepast

--- WachtrijApp/FormWachtrij.Designer.cs
+++ WachtrijApp/FormWachtrij.Designer.cs
@@ -41,30 +41,30 @@
             // labelTitel
             // 
             this.labelTitel.AutoSize = true;
-            this.labelTitel.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitel.Font = new System.Drawing.Font("Bell MT", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelTitel.Location = new System.Drawing.Point(26, 18);
             this.labelTitel.Name = "labelTitel";
-            this.labelTitel.Size = new System.Drawing.Size(186, 32);
+            this.labelTitel.Size = new System.Drawing.Size(170, 30);
             this.labelTitel.TabIndex = 0;
             this.labelTitel.Text = "Bugs of Horror";
             // 
             // labelWachttijd
             // 
             this.labelWachttijd.AutoSize = true;
-            this.labelWachttijd.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWachttijd.Font = new System.Drawing.Font("Cambria", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelWachttijd.Location = new System.Drawing.Point(27, 149);
             this.labelWachttijd.Name = "labelWachttijd";
-            this.labelWachttijd.Size = new System.Drawing.Size(99, 25);
+            this.labelWachttijd.Size = new System.Drawing.Size(104, 25);
             this.labelWachttijd.TabIndex = 4;
             this.labelWachttijd.Text = "Wachttijd";
             // 
             // labelWachttijdMelding
             // 
             this.labelWachttijdMelding.AutoSize = true;
-            this.labelWachttijdMelding.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWachttijdMelding.Font = new System.Drawing.Font("Calibri", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelWachttijdMelding.Location = new System.Drawing.Point(29, 174);
             this.labelWachttijdMelding.Name = "labelWachttijdMelding";
-            this.labelWachttijdMelding.Size = new System.Drawing.Size(71, 18);
+            this.labelWachttijdMelding.Size = new System.Drawing.Size(70, 18);
             this.labelWachttijdMelding.TabIndex = 5;
             this.labelWachttijdMelding.Text = "0 minuten";
             // 
@@ -82,20 +82,20 @@
             // labelKar2
             // 
             this.labelKar2.AutoSize = true;
-            this.labelKar2.Font = new System.Drawing.Font("Calibri", 11.25F);
+            this.labelKar2.Font = new System.Drawing.Font("Bell MT", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelKar2.Location = new System.Drawing.Point(29, 87);
             this.labelKar2.Name = "labelKar2";
-            this.labelKar2.Size = new System.Drawing.Size(57, 18);
+            this.labelKar2.Size = new System.Drawing.Size(64, 19);
             this.labelKar2.TabIndex = 8;
             this.labelKar2.Text = "Kar 2: ...";
             // 
             // labelKar1
             // 
             this.labelKar1.AutoSize = true;
-            this.labelKar1.Font = new System.Drawing.Font("Calibri", 11.25F);
+            this.labelKar1.Font = new System.Drawing.Font("Bell MT", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelKar1.Location = new System.Drawing.Point(29, 68);
             this.labelKar1.Name = "labelKar1";
-            this.labelKar1.Size = new System.Drawing.Size(57, 18);
+            this.labelKar1.Size = new System.Drawing.Size(64, 19);
             this.labelKar1.TabIndex = 9;
             this.labelKar1.Text = "Kar 1: ...";
             // 
@@ -103,7 +103,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Peru;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(279, 227);
             this.Controls.Add(this.labelKar1);
             this.Controls.Add(this.labelKar2);
@@ -111,9 +111,11 @@
             this.Controls.Add(this.labelWachttijdMelding);
             this.Controls.Add(this.labelWachttijd);
             this.Controls.Add(this.labelTitel);
+            this.Font = new System.Drawing.Font("Monotype Corsiva", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormWachtrij";
             this.Text = "Bugs of Horror";
+            this.Load += new System.EventHandler(this.FormWachtrij_Load);
             ((System.ComponentModel.ISupportInitialize)(this.AttractieLogo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
